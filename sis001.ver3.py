447b606db609a02f0e28fe40702507bc17b04d32
maxs98 <r3df0x@163.com>
2015-04-11 14:42:16 +0800

    fix bug

--- sis001.ver3.py
+++ sis001.ver3.py
@@ -14,7 +14,7 @@ class sis001:
 	def __init__(self): 
 		self.s = requests.Session()
 		self.browse_headers ={'User-Agent':'Mozilla/5.0 (Windows NT 5.1; rv:22.0) Gecko/20100101 Firefox/22.0'}
-		LOG_FILENAME="d:\\sis001\log.txt"
+		LOG_FILENAME="d:\\sis001\\log.txt"
 		logging.basicConfig(filename=LOG_FILENAME,format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 		self.forumurl = 'http://38.103.161.185'
 		'''
@@ -30,8 +30,8 @@ class sis001:
 		self.login()
 		
 	def getHash(self):
-		html = requests.get('http://38.103.161.185/forum/logging.php?action=login',headers=self.browse_headers)
-		hash = re.findall(b'<input type="hidden" name="formhash" value="(.*?)" />',html.content)
+		hash_html = requests.get('http://38.103.161.185/forum/logging.php?action=login',headers=self.browse_headers)
+		hash = re.findall(b'<input type="hidden" name="formhash" value="(.*?)" />',hash_html.content)
 		hash = hash.pop()
 		return hash
 
@@ -52,16 +52,16 @@ class sis001:
 		"loginsubmit":"true"}
 		
 		try:
-			html = self.s.post("%s/forum/logging.php?action=login&loginsubmit=true"%self.forumurl, data=login_info,headers=self.browse_headers,timeout=10)
-			login_success_info = re.findall(b'volstad',html.content,re.M|re.S)
+			login_html = self.s.post("%s/forum/logging.php?action=login&loginsubmit=true"%self.forumurl, data=login_info,headers=self.browse_headers,timeout=10)
+			login_success_info = re.findall(b'volstad',login_html.content,re.M|re.S)
 			if len(login_success_info) >= 1:
-				print('login success!!')
+				print('ok logging success!!')
 			else:
 				print('login fail!!')
 		except requests.exceptions.ConnectionError as e:
-			logging.info(e)
+			logging.error(e)
 		except requests.exceptions.HTTPError as e:
-			logging.info(e)
+			logging.error(e)
 			
 
 
@@ -75,12 +75,12 @@ class ThreadGetTids(threading.Thread):
 		
 	def run(self):
 		url = "%s/forum/forum-%s-%s.html"%(self.forumurl,self.fid,self.page)
-		html = sis001.s.get(url,headers=sis001.browse_headers,timeout=30)
-		if html.content == '':
+		page_html = sis001.s.get(url,headers=sis001.browse_headers,timeout=30)
+		if page_html.content == '':
 			return
 		attempts = 0
 		while attempts < 3:
-			topics_html = re.findall(b'<tbody.*?normalthread.*?>.*?</tbody>', html.content, re.M | re.S) #粗过滤，先过滤每个帖子的所有内容返回top_html只包含所有帖子的内容
+			topics_html = re.findall(b'<tbody.*?normalthread.*?>.*?</tbody>', page_html.content, re.M | re.S) #粗过滤，先过滤每个帖子的所有内容返回top_html只包含所有帖子的内容
 			if topics_html == '':
 				attempts += 1 # need retry download
 			else:
@@ -95,23 +95,23 @@ class ThreadGetTids(threading.Thread):
 				#print gd['title']
 				#logging.info(gd)
 		except Exception as e:
-			logging.info(e)
+			logging.error(e)
 		
 		for i in topics:
 			down_link_imgs_torrents(topics[i])
 		return topics
 	
 def down_link_imgs_torrents(topic):
-	print('GET:%s---%s'%(topic['url'],topic['title']))
+	print('GET:%s---%s'%(topic['url'],topic['title'].decode('gbk')))
 	dirname = get_valid_filename(topic['title'].decode('gbk')) #由于windows默认是gbk编码，建立文件夹时必须解码成gbk字符
 	#print dirname
 	if not os.path.exists(dirname):
 		os.makedirs(dirname)
 	topic_url = "%s/forum/%s"%(sis001.forumurl,topic['url'].decode('gbk'))
 	base_url = "%s/forum/"%(sis001.forumurl)
-	html = sis001.s.get(topic_url,headers=sis001.browse_headers,timeout=30)
-	imgs = re.findall(b'\<img src\=\"(http\:\/\/.*?\.jpg|attachments\/.*?\.jpg)\"', html.content, re.M | re.S) #匹配这个页面所有的图片链接
-	torrents = re.findall(b'a href=\"(?P<url>attach[=0-9a-zA-Z\.\?]+).*?>(?P<title>[^<>\"]*?torrent)', html.content, re.M | re.S) #匹配这个页面所有的种子链接
+	img_html = sis001.s.get(topic_url,headers=sis001.browse_headers,timeout=30)
+	imgs = re.findall(b'\<img src\=\"(http\:\/\/.*?\.jpg|attachments\/.*?\.jpg)\"', img_html.content, re.M | re.S) #匹配这个页面所有的图片链接
+	torrents = re.findall(b'a href=\"(?P<url>attach[=0-9a-zA-Z\.\?]+).*?>(?P<title>[^<>\"]*?torrent)', img_html.content, re.M | re.S) #匹配这个页面所有的种子链接
     
 	imgs = set(imgs)
 	st = set(torrents) 
@@ -139,21 +139,17 @@ def down_link(url, filename, istorrent = 0):
 	attempts = 0
 	while attempts < 10:
 		try:
-			html = sis001.s.get(url,headers=sis001.browse_headers,timeout=30)
+			#html = sis001.s.get(url,headers=sis001.browse_headers,timeout=30)
+			req = requests.Session()#新建连接来下载图片
+			save_html = req.get(url,headers=sis001.browse_headers,timeout=30)
+			if save_html.content == None:
+				return
+			open(filename, "wb").write(save_html.content)
 			break
 		except Exception as e:
 			attempts += 1
-			#print(e)
-			logging.info(e)
-	try:
-		if html.content is '':
-			return
-		logging.info(filename)
-		logging.info(url)
-		open(filename, "wb").write(html.content)
-	except Exception as e:
-		#print(e)
-		logging.info(e)
+			logging.error(e)			
+	logging.info(filename +"||"+ url)	
 	return
 
 def get_valid_filename(filename): #如果文件名有数字字母和特殊字符，按顺序连接起来去除右边空格，（连接c）
@@ -165,9 +161,9 @@ def get_valid_filename(filename): #如果文件名有数字字母和特殊字符
 if __name__ == '__main__':
 	start = time.time()
 	sis001 = sis001()
-	t = dict()	
-	fids = {"版面代码":25} #需要采集的版面{"典藏":411,"熟妇":242,"自拍":62,"若兰居":495,"亚洲无码":25}
-	pages = range(1,2) #采集的页数
+	t = dict()
+	fids = {"版面代码":143} #需要采集的版面{"典藏":411,"熟妇":242,"自拍":62,"若兰居":495,"亚洲无码转帖":25,"亚洲无码原创":143,"亚洲有码原创":230,"欧美无码原创":229,"欧美无码分享":77}
+	pages = range(1,11) #采集的页数
 	if not os.path.exists('av'):
 		os.makedirs('av')
 	os.chdir('av')
